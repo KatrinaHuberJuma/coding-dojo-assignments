5819ce14db02d2aaf8b23cf08f459bc380ee278a
Katrina Huber <Katrina@Katrina-Hubers-Computer.local>
2019-09-04 18:34:29 -0700

    playground assignment

---
+++ Python/flask/flask_fundamentals/playground/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+
+@app.route("/play/")
+@app.route("/<color>/<num>/")
+def repeat_name(num='3', color='blue'):
+    try:
+        num = int(num)
+    except ValueError:
+        return "please follow the format '.../color/int"
+
+    return render_template("index.html", color=color, num=num)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
