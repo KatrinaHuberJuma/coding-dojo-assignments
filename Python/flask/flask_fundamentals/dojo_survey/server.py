a25737aa76572bd437650c2bd4cd3eb112e43f2a
Katrina Huber <Katrina@Katrina-Hubers-Computer.local>
2019-09-08 08:19:13 -0700

    complete dojo survey assignment

--- Python/flask/flask_fundamentals/dojo_survey/server.py
+++ Python/flask/flask_fundamentals/dojo_survey/server.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, session, request, redirect
 
 app = Flask(__name__)
 
+
+app.secret_key = "akjsdhfadhsfkjashdfkjashfjkashfajklsfhakjsdhf"
+
 @app.route("/")
 def display_form():
     return render_template("index.html")
@@ -9,10 +12,15 @@ def display_form():
 @app.route("/process", methods=["POST"])
 def get_user_input():
     session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comment'] = request.form['comment']
+    return redirect("/result")
 
-@app.route("/")
+
+@app.route("/result")
 def display_result():
-    return render_template("result.html")
+    return render_template("result.html", name=session['name'], location=session['location'], language=session['language'], comment=session['comment'])
 
 
 if __name__ == "__main__":
