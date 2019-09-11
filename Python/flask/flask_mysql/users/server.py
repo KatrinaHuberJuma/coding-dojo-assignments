31a6a2c949ea98ad53721c8cf0cdc89e961a98cd
Katrina Huber <Katrina@Katrina-Hubers-Computer.local>
2019-09-10 19:57:12 -0700

    more routes added and fixed

--- Python/flask/flask_mysql/users/server.py
+++ Python/flask/flask_mysql/users/server.py
@@ -51,5 +51,40 @@ def add_user():
 
     return redirect(route_str)
 
+@app.route("/users/<user_id>/edit")
+def desplay_edit_page(user_id):
+    mysql = connectToMySQL('users')
+    query = "SELECT * FROM users WHERE id = %(id)s;"
+    data = {
+        'id': user_id
+    }
+    users = mysql.query_db(query, data)
+    user = users[0]
+    
+    return render_template("edit_user.html", user=user)
+
+@app.route("/users/<user_id>/update", methods=["PUT"] )
+def edit_user(user_id):
+    print("line 27______", user_id)
+    mysql = connectToMySQL('users')
+    query = """UPDATE users.users 
+            SET first_name=%(fn)s, last_name=%(ln)s, email=%(em)s, updated_at = NOW(), 
+            WHERE id = %(id)s;"""
+# UPDATE `users`.`users` SET `first_name` = 'Katrinahfvkjasfdh' WHERE (`id` = '4');
+
+    data = {
+        'id': user_id,
+        'fn': request.form['fname'],
+        'ln': request.form['lname'],
+        'em': request.form['email']
+    }
+    user_id = mysql.query_db(query, data)
+    print(user_id)
+    route_str = "/users/" + str(user_id)
+
+    return redirect(route_str)
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
