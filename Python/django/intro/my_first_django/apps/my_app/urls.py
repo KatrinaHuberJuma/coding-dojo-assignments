6804e3070842516431bb14c6e783d4c8fddade01
Katrina Huber <Katrina@Katrina-Hubers-Computer.local>
2019-09-11 11:20:14 -0700

    first django assignment

---
+++ Python/django/intro/my_first_django/apps/my_app/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from . import views
+                    
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^new$', views.new), 
+    url(r'^blogs$', views.index),
+    url(r'^create$', views.create),  
+    url(r'^(?P<blog_id>[0-9])$', views.show),  
+    url(r'^(?P<blog_id>[0-9]+)/edit$', views.edit),  
+    url(r'^(?P<blog_id>[0-9]+)/destroy$', views.destroy),  
+]                           # r'^$' - 
+                                # the rest of the route both starts and ends with nothing 
+                                # (i.e. "/" is the full route), and
+                            # views.index - 
+                                # if the requested route matches this pattern, 
+                                # then the function with the name "index" from 
+                                # this app's views.py file will be invoked.
+                            # Note: ALL route patterns at the app level should end with $.
---
+++ Python/django/intro/my_first_django/apps/my_app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("this is the equivalent of @app.route('/')!")
+
+
+def new(request):
+    return HttpResponse("NEW THING OMG!")
+
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+
+def create(request):
+    # print("+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**+*+*+*+")
+    response = redirect('/blogs')
+    return response
+
+
+def show(request, blog_id):
+    # print("+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**+*+*+*+")
+    url_string = "placeholder to later display blog number: ", str(blog_id)
+    return HttpResponse(url_string) # TODO: why is this a different font?
+
+
+def edit(request, blog_id):
+    # print("+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**+*+*+*+")
+    url_string = "placeholder to later EDIT blog number: ", str(blog_id)
+    return HttpResponse(url_string) # TODO: why is this a different font?
+
+def destroy(request, blog_id):
+    print("+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+"+ str(blog_id)+"*+*+*+*+*+*+*+*+*+*+*+*+**+*+*+*+")
+    response = redirect('/')
+    return response
