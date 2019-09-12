094e32e40f5e7ad527ae2768ee8f0d25e5e43208
Katrina Huber <Katrina@Katrina-Hubers-Computer.local>
2019-09-12 16:28:04 -0700

    minor improvements

--- Python/django/intro/ninja_gold/apps/gold_dig/views.py
+++ Python/django/intro/ninja_gold/apps/gold_dig/views.py
@@ -15,13 +15,13 @@ def process_money(request):
     #     return render(request, "gold_dig/index.html")
     if request.method == "POST":
         if request.POST['location'] == 'farm':
-            request.session['gold'] += random.choice([10, 15, 20])
+            request.session['gold'] += random.randint(10, 20)
         elif request.POST['location'] == 'cave':
-            request.session['gold'] += random.choice([5, 10])
+            request.session['gold'] += random.randint(5, 10)
         elif request.POST['location'] == 'house':
-            request.session['gold'] += random.choice([2, 3, 4, 5])
+            request.session['gold'] += random.randint(2, 5)
         else:
-            request.session['gold'] += random.choice([20, -20])
+            request.session['gold'] += random.randint(-20, 20)
         return redirect("/")
 
 
