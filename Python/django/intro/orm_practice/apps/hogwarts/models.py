bc92112456a5daaa9a29c77f2506cd401556f3fe
Katrina Huber <Katrina@Katrina-Hubers-Computer.local>
2019-09-13 08:31:48 -0700

    second orm practice assignment

--- Python/django/intro/orm_practice/apps/hogwarts/models.py
+++ Python/django/intro/orm_practice/apps/hogwarts/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
-# Create your models here.
-class Hogwarts(models.Model):
-    name = models.CharField(max_length=45)
-    house = models.CharField(max_length=45)
-    pet = models.CharField(max_length=45)
-    year = models.IntegerField()
+# # Create your models here.
+# class Hogwarts(models.Model):
+#     name = models.CharField(max_length=45)
+#     house = models.CharField(max_length=45)
+#     pet = models.CharField(max_length=45)
+#     year = models.IntegerField()
+
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    def __repr__(self):
+        return f"<User object: {self.first_name} {self.last_name}({self.email_address})>"
